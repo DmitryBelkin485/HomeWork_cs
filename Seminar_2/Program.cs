73ffabddc69990e92bb613ef0ffa345785717c9f
Belkin Dmitry <dmitry485@gmail.com>
2022-11-04 23:26:50 +0500

    add case 2

--- Seminar_2/Program.cs
+++ Seminar_2/Program.cs
@@ -15,7 +15,7 @@
 
 
 
-// // слегка друой вариант решения._ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
+// // Другой вариант решения._ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
 // Console.Write("Введите 3х значное число: ");
 // int number = Convert.ToInt32(Console.ReadLine());
 // int amount = number.ToString().Length;
@@ -40,4 +40,37 @@
 
 
 // // Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// Console.Clear();
+// Console.Write("Введите 3х значное число: ");
+// string number = Console.ReadLine();
+// int amount = number.Length;
 
+// if (amount < 3 || amount > 1000)
+// {
+//     Console.WriteLine("Третьей цифры нет");
+// }
+// else
+// {
+//     Console.WriteLine($"Вывод : {number[2]}");
+// }
+
+// // Другой вариант решения._ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
+
+// Console.Clear();
+
+// bool Third(string number)
+// {
+//     bool result = int.TryParse(number, out int Num);
+//     return result;
+// }
+
+// Console.Write("Введите число: ");
+// string number = Console.ReadLine();
+
+// while (!Third(number))
+// {
+//     Console.Write("Вы ввели не число. Поробуйте снова: ");
+//     number = Console.ReadLine();
+// }
+// Console.WriteLine((number.Length > 2 ? $"Третья цифра числа: {number[2]}" : "Без третьей цифры"));
+// //_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
