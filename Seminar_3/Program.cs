08c8945e645005c920a2eb40f3cf9d392505fdf2
Belkin Dmitry <dmitry485@gmail.com>
2022-11-10 00:18:09 +0500

    add case 1

--- Seminar_3/Program.cs
+++ Seminar_3/Program.cs
@@ -1,1 +1,25 @@
-﻿
+﻿// Задача 23: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 5 -> 1,8,27,64,125
+
+void sqrt(int N)
+{
+    if (N > 0)
+    {
+        int count = 1;
+        while (count < N)
+        {
+            int result = count * count * count;
+            Console.Write(result + ", ");
+            count++;
+        }
+        Console.Write(N * N * N);
+    }
+    else
+    {
+        Console.WriteLine("Введите число больше 1");
+    }
+}
+Console.WriteLine("enter number: ");
+int N = Convert.ToInt32(Console.ReadLine());
+sqrt(N);
+
