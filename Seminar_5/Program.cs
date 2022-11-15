8a0ec82bce17c665fd51ce61f7d07e879e798138
Belkin Dmitry <dmitry485@gmail.com>
2022-11-15 20:50:35 +0500

    add changes

--- Seminar_5/Program.cs
+++ Seminar_5/Program.cs
@@ -71,51 +71,51 @@
 // // Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементами массива.
 // // [3 7 22 2 78] -> 76
 
-Console.Clear();
+// Console.Clear();
 
-Console.Write("Enter the size of the array: ");
-int size = int.Parse(Console.ReadLine());
-int[] numbers = new int[size];
-FillArrayRandomNumbers(numbers);
-PrintArray(numbers);
+// Console.Write("Enter the size of the array: ");
+// int size = int.Parse(Console.ReadLine());
+// int[] numbers = new int[size];
+// FillArrayRandomNumbers(numbers);
+// PrintArray(numbers);
 
-int max = numbers[0];
-int min = numbers[0];
+// int max = numbers[0];
+// int min = numbers[0];
 
-for (int i = 0; i < numbers.Length; i++)
-{
-    if (numbers[i] > max)
-    {
-        max = numbers[i];
-    }
-    else if (numbers[i] < min)
-    {
-        min = numbers[i];
-    }
-}
+// for (int i = 1; i < numbers.Length; i++)
+// {
+//     if (numbers[i] > max)
+//     {
+//         max = numbers[i];
+//     }
+//     else if (numbers[i] < min)
+//     {
+//         min = numbers[i];
+//     }
+// }
 
-Console.WriteLine($"Minimum number: {min}");
-Console.WriteLine($"Maximum number: {max}");
-Console.WriteLine($"Difference between maximum and minimum numbers: {max - min}");
-
-
-
-void FillArrayRandomNumbers(int[] array)
-{
-    for (int i = 0; i < array.Length; i++)
-    {
-        array[i] = new Random().Next(1, 1000);
-    }
-}
+// Console.WriteLine($"Minimum number: {min}");
+// Console.WriteLine($"Maximum number: {max}");
+// Console.WriteLine($"Difference between maximum and minimum numbers: {max - min}");
 
 
-void PrintArray(int[] array)
-{
-    Console.Write("[");
-    for (int i = 0; i < array.Length; i++)
-    {
-        Console.Write($"{array[i]} ");
-    }
-    Console.Write("]");
-    Console.WriteLine();
-}
+
+// void FillArrayRandomNumbers(int[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         array[i] = new Random().Next(1, 1000);
+//     }
+// }
+
+
+// void PrintArray(int[] array)
+// {
+//     Console.Write("[");
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         Console.Write($"{array[i]} ");
+//     }
+//     Console.Write("]");
+//     Console.WriteLine();
+// }
