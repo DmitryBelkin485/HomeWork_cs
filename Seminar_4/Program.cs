782ba754b0c8e54ee3c372ca546da0f6d4dd2618
Belkin Dmitry <dmitry485@gmail.com>
2022-11-16 23:05:06 +0500

    add changes

--- Seminar_4/Program.cs
+++ Seminar_4/Program.cs
@@ -75,16 +75,16 @@
 // Console.WriteLine($"[{String.Join(",", array)}]");
 // // _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
 
-Console.Clear();
-int[] array = GetRandomArray(8, 0, 100);
-Console.WriteLine($"[{String.Join(",", array)}]");
+// Console.Clear();
+// int[] array = GetRandomArray(8, 0, 100);
+// Console.WriteLine($"[{String.Join(",", array)}]");
 
-int[] GetRandomArray(int size, int minValue, int maxValue)
-{
-    int[] result = new int[size];
-    for (int i = 0; i < size; i++)
-    {
-        result[i] = new Random().Next(minValue, maxValue);
-    }
-    return result;
-}
+// int[] GetRandomArray(int size, int minValue, int maxValue)
+// {
+//     int[] result = new int[size];
+//     for (int i = 0; i < size; i++)
+//     {
+//         result[i] = new Random().Next(minValue, maxValue);
+//     }
+//     return result;
+// }
