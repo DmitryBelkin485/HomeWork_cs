8a8a18eb86263f2a0508ccac4d0615d3c4c44092
Belkin Dmitry <dmitry485@gmail.com>
2022-11-22 19:06:03 +0500

    add Task_41

--- Seminar_6/Program.cs
+++ Seminar_6/Program.cs
@@ -1,1 +1,45 @@
-﻿
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 4Console.Write("Введите элементы(через пробел): ");
+
+
+// int[] arr = Array.ConvertAll(Console.ReadLine().Split(), Convert.ToInt32); //int.Parse
+// int count = 0;
+ 
+// for (int i = 0; i < arr.Length; i++)
+// {
+//     if (arr[i] > 0)
+//     {
+//         count++;
+//     }
+// }
+ 
+// Console.WriteLine($"Кол-во элементов > 0: {count}");
+
+// //Другой вариант:_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ 
+
+// int[] arr = CreateRandomArray(5, -100, 100);
+// Console.WriteLine(String.Join(" ", arr));
+// int count = 0;
+// for (int i = 0; i < arr.Length; i++)
+// {
+//     if (arr[i] > 0)
+//     {
+//         count++;
+//     }
+// }
+
+// Console.WriteLine($"Кол-во элементов > 0: {count}");
+
+
+// int[] CreateRandomArray(int N, int start, int end)
+// {
+//     int[] RandomArray = new int[N];
+//     for (int i = 0; i < N; i++)
+//     {
+//         RandomArray[i] = new Random().Next(start, end + 1);
+//     }
+//     return RandomArray;
+// }
