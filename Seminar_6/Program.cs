a54baeac5d3f32c18432160a4f3c7c5d383cf8d9
Belkin Dmitry <dmitry485@gmail.com>
2022-11-22 19:31:07 +0500

    add task_43

--- Seminar_6/Program.cs
+++ Seminar_6/Program.cs
@@ -2,12 +2,14 @@
 
 // 0, 7, 8, -2, -2 -> 2
 
-// 1, -7, 567, 89, 223-> 4Console.Write("Введите элементы(через пробел): ");
+// 1, -7, 567, 89, 223-> 4
 
+// Console.Clear();
 
+// Console.Write("Введите элементы(через пробел): ");
 // int[] arr = Array.ConvertAll(Console.ReadLine().Split(), Convert.ToInt32); //int.Parse
 // int count = 0;
- 
+
 // for (int i = 0; i < arr.Length; i++)
 // {
 //     if (arr[i] > 0)
@@ -15,10 +17,12 @@
 //         count++;
 //     }
 // }
- 
+
 // Console.WriteLine($"Кол-во элементов > 0: {count}");
 
 // //Другой вариант:_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ 
+
+// Console.Clear();
 
 // int[] arr = CreateRandomArray(5, -100, 100);
 // Console.WriteLine(String.Join(" ", arr));
@@ -43,3 +47,28 @@
 //     }
 //     return RandomArray;
 // }
+
+// // Задача 43. Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+// // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; 0,5)
+
+Console.Clear();
+
+Console.Write("Введите k1: ");
+var k1 = double.Parse(Console.ReadLine()); //Convert.ToDouble(Console.ReadLine())
+Console.Write("Введите b1: ");
+var b1 = double.Parse(Console.ReadLine()); //Convert.ToDouble(Console.ReadLine())
+Console.Write("Введите k2: ");
+var k2 = double.Parse(Console.ReadLine()); //Convert.ToDouble(Console.ReadLine())
+Console.Write("Введите b2: ");
+var b2 = double.Parse(Console.ReadLine()); //Convert.ToDouble(Console.ReadLine())
+
+var x = -(b1 - b2) / (k1 - k2);
+var y = k1 * x + b1;
+
+x = Math.Round(x, 3);
+y = Math.Round(y, 3);
+
+Console.WriteLine($"Пересечение в точке: ({x};{y})");
+
+
