5ddd1186b8fe2cab0c25c4a8451edcb2a3069c55
Mr__Moore <dmitry485@gmail.com>
2022-12-01 22:25:23 +0500

    add task 47

--- Seminar_7/Program.cs
+++ Seminar_7/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// // Задача 47: Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.?
+// Console.Clear();
+
+// Console.WriteLine("Введите количество строк: ");
+// int m = int.Parse(Console.ReadLine());
+// Console.WriteLine("Введите количество столбцов: ");
+// int n = int.Parse(Console.ReadLine());
+
+// double[,] numbers = new double[m, n];
+// Random rnd = new Random();
+
+// FillArray(numbers);
+// Console.WriteLine();
+// PrintArray(numbers);
+
+// void PrintArray(double[,] matr)
+// {
+//     for (int i = 0; i < m; i++)
+//     {
+//         for (int j = 0; j < n; j++)
+//         { Console.Write($"{matr[i, j]} "); }
+//         Console.WriteLine();
+//     }
+// }
+
+// void FillArray(double[,] matr)
+// {
+//     for (int i = 0; i < m; i++)
+//     {
+//         for (int j = 0; j < n; j++)
+//             { matr[i, j] = Convert.ToDouble(rnd.Next(-100, 100) / 10.0); }
+//     }
+// }
+
