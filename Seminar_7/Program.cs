aec8e1944720d53666ee0668c549e8c8e2092f68
Mr__Moore <dmitry485@gmail.com>
2022-12-02 21:37:49 +0500

    add changes

--- Seminar_7/Program.cs
+++ Seminar_7/Program.cs
@@ -82,54 +82,54 @@
 
 
 // // Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
-Console.Clear();
+// Console.Clear();
 
-Console.Write("введите количество строк: ");
-int n = int.Parse(Console.ReadLine());
-Console.Write("введите количество столбцов: ");
-int m = int.Parse(Console.ReadLine());
+// Console.Write("введите количество строк: ");
+// int n = int.Parse(Console.ReadLine());
+// Console.Write("введите количество столбцов: ");
+// int m = int.Parse(Console.ReadLine());
 
-int[,] numbers = new int[n, m];
-FillArrayRandomNumbers(numbers);
-
-
-for (int j = 0; j < numbers.GetLength(1); j++)
-{
-    double avarage = 0;
-    for (int i = 0; i < numbers.GetLength(0); i++)
-    {
-        avarage = (avarage + numbers[i, j]);
-    }
-    avarage = avarage / n;
-    Console.Write(avarage + "; ");
-}
-Console.WriteLine();
-PrintArray(numbers);
+// int[,] numbers = new int[n, m];
+// FillArrayRandomNumbers(numbers);
 
 
+// for (int j = 0; j < numbers.GetLength(1); j++)
+// {
+//     double avarage = 0;
+//     for (int i = 0; i < numbers.GetLength(0); i++)
+//     {
+//         avarage = (avarage + numbers[i, j]);
+//     }
+//     avarage = avarage / n;
+//     Console.Write(avarage + "; ");
+// }
+// Console.WriteLine();
+// PrintArray(numbers);
 
-void FillArrayRandomNumbers(int[,] array)
-{
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            array[i, j] = new Random().Next(0, 10);
-        }
-    }
-}
 
-void PrintArray(int[,] array)
-{
 
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        Console.Write("[ ");
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            Console.Write($"{array[i ,j]}  ");
-        }
-        Console.Write("]");
-        Console.WriteLine();
-    }
-}
+// void FillArrayRandomNumbers(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             array[i, j] = new Random().Next(0, 10);
+//         }
+//     }
+// }
+
+// void PrintArray(int[,] array)
+// {
+
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         Console.Write("[ ");
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             Console.Write($"{array[i ,j]}  ");
+//         }
+//         Console.Write("]");
+//         Console.WriteLine();
+//     }
+// }
