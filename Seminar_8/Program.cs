0c98e7a48cff650c924cb569e1881689f340e086
Mr__Moore <dmitry485@gmail.com>
2022-12-02 23:23:25 +0500

    add task 58

--- Seminar_8/Program.cs
+++ Seminar_8/Program.cs
@@ -68,36 +68,121 @@ Console.Clear();
 
 // //Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
 
-int[,] table = new int[4, 4];
-FillArrayRandom(table);
-PrintArray(table);
+// int[,] table = new int[4, 4];
+// FillArrayRandom(table);
+// PrintArray(table);
+// Console.WriteLine();
+// NumberRowMinSumElements(table);
+
+
+// void NumberRowMinSumElements(int[,] array)
+// {
+//     int minRow = 0;
+//     int minSumRow = 0;
+//     int sumRow = 0;
+//     for (int i = 0; i < table.GetLength(1); i++)
+//     {
+//         minRow += table[0, i];
+//     }
+//     for (int i = 0; i < table.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < table.GetLength(1); j++)
+//         {
+//             sumRow += table[i, j];
+//         }
+//         if (sumRow < minRow)
+//         {
+//             minRow = sumRow;
+//             minSumRow = i;
+//         }
+//         sumRow = 0;
+//     }
+//     Console.Write($"Минимальная сумма элементов в: [{minSumRow + 1} строке]");
+// }
+
+// void PrintArray(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             Console.Write($"{array[i, j]} ");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+// void FillArrayRandom(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             array[i, j] = new Random().Next(1, 10);
+//         }
+//     }
+// }
+
+// Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+// Например, даны 2 матрицы:
+// 2 4 | 3 4
+// 3 2 | 3 3
+// Результирующая матрица будет:
+// 18 20
+// 15 18
+
+int rows = ReadInt("Введите количество строк: ");
+int columns = ReadInt("Введите количество столбцов: ");
+int[,] array = new int[rows, columns];
+int[,] secondArray = new int[rows, columns];
+int[,] resultArray = new int[rows, columns];
+
+FillArrayRandom(array);
+PrintArray(array);
+
 Console.WriteLine();
-NumberRowMinSumElements(table);
 
+FillArrayRandom(secondArray);
+PrintArray(secondArray);
 
-void NumberRowMinSumElements(int[,] array)
+Console.WriteLine();
+
+if (array.GetLength(0) != secondArray.GetLength(1))
 {
-    int minRow = 0;
-    int minSumRow = 0;
-    int sumRow = 0;
-    for (int i = 0; i < table.GetLength(1); i++)
+    Console.WriteLine(" Нельзя перемножить ");
+    return;
+}
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < secondArray.GetLength(1); j++)
     {
-        minRow += table[0, i];
+        resultArray[i, j] = 0;
+        for (int k = 0; k < array.GetLength(1); k++)
+        {
+            resultArray[i, j] += array[i, k] * secondArray[j, k]; 
+        }
     }
-    for (int i = 0; i < table.GetLength(0); i++)
+}
+
+PrintArray(resultArray);
+
+
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void FillArrayRandom(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
     {
-        for (int j = 0; j < table.GetLength(1); j++)
+        for (int j = 0; j < array.GetLength(1); j++)
         {
-            sumRow += table[i, j];
+            array[i, j] = new Random().Next(1, 10);
         }
-        if (sumRow < minRow)
-        {
-            minRow = sumRow;
-            minSumRow = i;
-        }
-        sumRow = 0;
     }
-    Console.Write($"Минимальная сумма элементов в: [{minSumRow + 1} строке]");
 }
 
 void PrintArray(int[,] array)
@@ -109,16 +194,5 @@ void PrintArray(int[,] array)
             Console.Write($"{array[i, j]} ");
         }
         Console.WriteLine();
-    }
-}
-
-void FillArrayRandom(int[,] array)
-{
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            array[i, j] = new Random().Next(1, 10);
-        }
     }
 }
